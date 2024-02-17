7514f504c524e3420f303ee19fe552a438983aae
Victor Leyva <vic.leyva5@gmail.com>
2024-02-17 05:42:12 -0600

    Handle values over 100 in progress bar

--- src/components/pages/PokemonPage/Progress/Progress.js
+++ src/components/pages/PokemonPage/Progress/Progress.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Progress = ({ value }) => {
-    const normalizedValue = Math.min(100, Math.max(0, value));
+    const normalizedValue = Math.min(200, Math.max(0, value)) / 2; // Scale down to the range [0, 100]
+
     return (
         <div className="relative h-2 w-full overflow-hidden rounded-full bg-blue-200">
             <div
